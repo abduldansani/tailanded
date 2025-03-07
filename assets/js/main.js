849690974b8b0cb9ea3a0f58ff47dc551bab0f6f
abduldansani <abduldansani@gmail.com>
2025-03-08 00:54:20 +0100

    responsiveness fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ const menuToggle = document.querySelector(".header__menuBtn");
 const menuIcon = document.querySelector(".header__menuIcon");
 const mobileMenu = document.querySelector(".menu");
 const body = document.body; // Select the body element
+const navLinks = document.querySelectorAll(".menu__navLink");
 
 menuToggle.addEventListener("click", () => {
   const isOpen = mobileMenu.classList.toggle("open");
@@ -9,6 +10,14 @@ menuToggle.addEventListener("click", () => {
 
   // Disable or enable scrolling
   body.style.overflow = isOpen ? "hidden" : "auto";
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("open");
+    menuIcon.classList.remove("open");
+    body.style.overflow = "auto"; // Enable scrolling again
+  });
 });
 
 const copyrightElement = document.querySelector(".footer__copyright");
